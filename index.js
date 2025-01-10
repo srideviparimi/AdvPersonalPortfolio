dfeae7513a81d571f0298efd3cdcff564e3271d2
Sridevi <parimisridevi586@gmail.com>
2025-01-10 17:30:37 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+//nav bar is visible only if the user scrolls upward and will not show it on downward scroll
+let prevScrollPosition=window.pageYOffset;
+window.addEventListener("scroll",function(){
+    const currentScrollPosition=window.pageYOffset;
+    if(prevScrollPosition>currentScrollPosition){
+        document.querySelector('.navbar').classList.add("fixed-top");//user scrolled up
+    }
+    else{
+       document.querySelector('.navbar').classList.remove("fixed-top");//user scrolled down
+    }
+    prevScrollPosition=currentScrollPosition;
+});
+// this is the background for the home section
+tsParticles.load("tsparticles1",{
+        particles:{
+            move:{
+                enable:true
+            },
+            links:{
+                color:"#fffa77",
+                enable:true,
+                opacity:0.5,
+                width:1
+            },
+            shape:{
+                type:"triangle",
+                
+            }
+        }
+    });
+      //this is the background for the skills section
+    tsParticles.load("tsparticles2",{
+        particles:{
+            move:{
+                enable:true,
+                speed: 3,
+                direction: "top",
+            },
+            shape:{
+                type:"circle"
+            },
+            color:{
+               value: ["#355070","#6d597a","#b56576"]
+            }
+        }
+    })
+   
+    tsParticles.load({
+        id: "tsparticles3",
+        options: {
+          particles: {
+            shape: {
+              type: "square", // starting from v2, this require the square shape script
+            },
+          },
+          preset: "fireworks",
+        },
+      });
+    
+// this is for the projects div in which the div kind of grows into a full size upon scrolling
+     var aDiv=document.getElementById("animatedDiv");
+     function changeWidth() 
+{
+    var scrollVal = window.pageYOffset;
+    var scrollSlow  = (scrollVal / 5);
+    
+    //Changing CSS Width
+    aDiv.style.width= Math.min(Math.max(scrollSlow, 20), 100) + "%";
+}
+
+window.addEventListener('scroll', function() 
+{
+    requestAnimationFrame(changeWidth);
+}, false);
+// we are adding the onclick events for the demos to open them in a new tab rather than the same one
+
+const rebelDemo=()=>{
+    window.open("https://srideviparimi.github.io/Rebel/","_blank")
+}
+const newsDemo=()=>{
+    window.open("https://news-fetching-application.netlify.app/","_blank")
+}
+/*document.getElementById("moreProjects").addEventListener("click",()=>{
+    console.log("clicked")
+    window.location.href="projects.html";
+})
+ const more=()=>{
+    window.open("./projects.html","_blank");
+}*/
+//event listener for the form successful submit
+const formElement=document.getElementById("contactform");
+formElement.addEventListener("submit",resettingForm());
+function resettingForm(e){
+   
+    formElement.reset();
+    //e.preventDefault();
+    
+
+
+}
+
---
+++ projects.js
@@ -0,0 +1,30 @@
+const rebelDemo=()=>{
+    window.open("https://srideviparimi.github.io/Rebel/","_blank")
+}
+const rebelGit=()=>{
+    window.open("https://github.com/srideviparimi/Rebel","_blank");
+}
+const newsDemo=()=>{
+    window.open("https://news-fetching-application.netlify.app/","_blank")
+}
+const newsGit=()=>{
+    window.open("https://github.com/srideviparimi/News-Fetching-Application","_blank");
+}
+const sPortfolioDemo=()=>{
+    window.open("https://srideviparimi-personalportfolio.netlify.app/","_blank");
+}
+const sPortfolioGit=()=>{
+    window.open("https://github.com/srideviparimi/Personal-Portfolio","_blank");
+}
+const pokemonDemo=()=>{
+    window.open("https://srideviparimi.github.io/pokemon-search-app/","_blank");  
+}
+const pokemonGit=()=>{
+    window.open("https://github.com/srideviparimi/pokemon-search-app","_blank");  
+}
+const calcDemo=()=>{
+    window.open("https://srideviparimi.github.io/Calculator/","_blank");
+}
+const calcGit=()=>{
+    window.open("https://github.com/srideviparimi/Calculator","_blank");
+}
